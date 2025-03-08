76ab61f1af129a3481d71793deb07c671e25bbab
rohitswami16 <rohitswami1612@gmail.com>
2025-03-09 01:40:58 +0530

    2 Project done

--- Project01_Color_Changer/index.js
+++ Project01_Color_Changer/index.js
@@ -32,3 +32,25 @@ buttons.forEach(function(btn) {
         }
     })  
 })
+
+
+// const buttons = document.querySelectorAll('.button');
+// console.log(buttons);
+
+// const body = document.querySelector('body');
+
+// buttons.forEach(function (button) {
+//   button.addEventListener('click', function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === 'grey') {
+//       body.style.backgroundColor = e.target.id;
+//     } else if (e.target.id === 'yellow') {
+//       body.style.backgroundColor = e.target.id;
+//     } else if (e.target.id === 'blue') {
+//       body.style.backgroundColor = e.target.id;
+//     } else {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//   });
+// });
