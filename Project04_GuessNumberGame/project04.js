380c93a599e9dbc0f9abb6997db429affa204906
rohitswami16 <rohitswami1612@gmail.com>
2025-03-10 17:47:50 +0530

    Projects

--- Project04_GuessNumberGame/project04.js
+++ Project04_GuessNumberGame/project04.js
@@ -23,7 +23,6 @@ if (playGame) {
     submit.addEventListener('click', function (e) {
         e.preventDefault();
         const guess = parseInt(userInput.value);
-        // console.log(guess);
         validateGuess(guess);
     });
 }
