2251747cb52e632e41b714bc53fd1deffe70bdbd
rohitswami16 <rohitswami1612@gmail.com>
2025-03-11 22:28:52 +0530

    OOP

--- 09_classes_and_oop/oop.js
+++ 09_classes_and_oop/oop.js
@@ -1,13 +1,31 @@
 const user = {
-    "name": "John",
+    name: "John",
     loginCount: 8,
     signedIn: true,
 
-    getUserDetails: function() {
-        console.log("got user deatils");
-        
+    getUserDetails: function () {
+        // console.log("got user deatils");
+        // console.log(`username: ${this.name}`);
+        // console.log(this);
     }
 }
 
+// console.log(this); // {}
+// user.getUserDetails()
+// const promiseOne = new Promise()
 
-console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+    // return this
+}
+
+const userOne = new User("ABC", 12, true)
+const userTwo = new User("DEF", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
+
+
