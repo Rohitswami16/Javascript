2132d1c2924977329766c7d38cc6d29323552f63
rohitswami16 <rohitswami1612@gmail.com>
2025-03-13 13:58:17 +0530

    call

---
+++ 09_classes_and_oop/call.js
@@ -0,0 +1,17 @@
+function SetUserName(username) {
+    
+    // it will set to the oen this (SetUserName)
+    this.username = username
+    console.log("callled");
+    
+}
+
+function creatUser(username, email, password){
+    SetUserName.call(this, username) // it will hold the reference 
+    this.email = email
+    this.password = password
+}
+
+const chai = new creatUser("abc", 'chai@gmail.com', '123')
+console.log(chai);
+
